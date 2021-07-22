9a63ca58cb7251c37c83c1d8e3ea3febfd6de4d4
Dashadower <tttllshin@gmail.com>
2021-07-22 11:25:25 +0900

    Add nsight systems and power profiling

--- run.py
+++ run.py
@@ -10,38 +10,59 @@ Wall time provided for sanity but is not a sane benchmark measurement.
 """
 import argparse
 import time
+import multiprocessing
+from datetime import datetime
+import csv
 import torch.autograd.profiler as profiler
-
+from py3nvml import py3nvml
 from torchbenchmark import list_models
 
 import torch
+
+
+def device_power_profiler(connection):
+    py3nvml.nvmlInit()
+    handle = nvmlDeviceGetHandleByIndex(0)
+    header = ["elapsed_time", "power"]
+    with open("/workspace/benchmark/ncu_reports/power_output.csv", "w") as f:
+        print("power profiler ready")
+        writer = csv.writer(f)
+        writer.writerow(header)
+        flag = connection.recv()
+        t0 = time.time()
+        while not connection.poll():
+            writer.writerow([str(time.time() - t0), nvmlDeviceGetPowerUsage(handle)])
+
+
 
 def run_one_step(func):
 
     func()
 
     if args.device == "cuda":
-      torch.cuda.synchronize()
-      start_event = torch.cuda.Event(enable_timing=True)
-      end_event = torch.cuda.Event(enable_timing=True)
+        
+        torch.cuda.synchronize()
+        start_event = torch.cuda.Event(enable_timing=True)
+        end_event = torch.cuda.Event(enable_timing=True)
 
-      start_event.record()
+        start_event.record()
 
-      t0 = time.time()
-      func()
-      t1 = time.time()
+        
+        t0 = time.time()
+        func()
+        t1 = time.time()
 
-      end_event.record()
-      torch.cuda.synchronize()
-      print(f"Ran in {t1 - t0} seconds, gpu time {start_event.elapsed_time(end_event)}.")
+        end_event.record()
+        torch.cuda.synchronize()
+        print(f"Ran in {t1 - t0} seconds, gpu time {start_event.elapsed_time(end_event)}.")
 
     else:
 
-      t0 = time.time()
-      func()
-      t1 = time.time()
+        t0 = time.time()
+        func()
+        t1 = time.time()
 
-      print(f"Ran in {t1 - t0} seconds.")
+        print(f"Ran in {t1 - t0} seconds.")
 
 
 def profile_one_step(func, nwarmup=3):
@@ -49,10 +70,15 @@ def profile_one_step(func, nwarmup=3):
         func()
 
     use_cuda = args.device == "cuda"
-
+    parent_conn, child_conn = multiprocessing.Pipe()
+    proc = multiprocessing.Process(device_power_profiler, (child_conn, ))
+    proc.start()
+    print("power profiler kicked off")
+    parent_conn.send("start")
     with profiler.profile(record_shapes=True, use_cuda = use_cuda) as prof:
         func()
 
+    parent_conn.send("stop")
     print(prof.key_averages(group_by_input_shape=True).table(sort_by="cpu_time_total", row_limit=30))
 
 if __name__ == "__main__":
@@ -63,7 +89,6 @@ if __name__ == "__main__":
     parser.add_argument("-t", "--test", choices=["eval",  "train"], default="eval", help="Which test to run.")
     parser.add_argument("--profile", action="store_true", help="Run the profiler around the function")
     args = parser.parse_args()
-
     found = False
     for Model in list_models():
         if args.model.lower() in Model.name.lower():
